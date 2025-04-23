1e25a099b412aee2468be6e5c91c5734ebb4d540
Svilyear <villiersteven@gmail.com>
2025-04-23 11:15:15 +0100

    updated

--- app.js
+++ app.js
@@ -43,32 +43,32 @@ if (isVercel) {
 }
 
 // Route to handle form submission
-app.post('/submit', (req, res) => {
+app.post('/api/submit', (req, res) => {
   const { name, email, message } = req.body;
 
-  // Email options
-  const mailOptions = {
-    // from: EMAIL_USER,
-    // to: EMAIL_USER, // Send to yourself
-    // replyTo: email,
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    replyTo: email,
-    subject: `New Contact Form Submission from ${name}`,
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    html: `
-      <h2>New Contact Form Submission</h2>
-      <p><strong>Name:</strong> ${name}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Message:</strong></p>
-      <p>${message.replace(/\n/g, '<br>')}</p>
-    `
-  };
+  // // Email options
+  // const mailOptions = {
+  //   // from: EMAIL_USER,
+  //   // to: EMAIL_USER, // Send to yourself
+  //   // replyTo: email,
+  //   from: process.env.EMAIL_USER,
+  //   to: process.env.EMAIL_USER,
+  //   replyTo: email,
+  //   subject: `New Contact Form Submission from ${name}`,
+  //   text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  //   html: `
+  //     <h2>New Contact Form Submission</h2>
+  //     <p><strong>Name:</strong> ${name}</p>
+  //     <p><strong>Email:</strong> ${email}</p>
+  //     <p><strong>Message:</strong></p>
+  //     <p>${message.replace(/\n/g, '<br>')}</p>
+  //   `
+  // };
 
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-    //   // console.error('Error sending email:', error);
+  // // Send email
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //   //   // console.error('Error sending email:', error);
     //   // return res.status(500).json({ 
     //     // success: false, 
     //     // message: 'Error sending your message. Please try again later.' 
@@ -82,12 +82,12 @@ app.post('/submit', (req, res) => {
     // //   success: true, 
     // //   message: 'Thank you for your message! I will get back to you soon.' 
     // res.sendFile(path.join(__dirname, 'contact.html'));
-    console.error('Error:', error);
-      return res.redirect(303, '/public/404.html');
-    }
-    return res.redirect(303, '/public/contact.html');
-    });
-  });
+  //   console.error('Error:', error);
+  //     return res.redirect(303, '/public/404.html');
+  //   }
+  //   return res.redirect(303, '/public/contact.html');
+  //   });
+  // });
 
 
 
