c12886dc78f2b51769e816c3a81fca0eeefaaa63
Svilyear <villiersteven@gmail.com>
2025-04-23 11:17:29 +0100

    updated

--- app.js
+++ app.js
@@ -46,29 +46,29 @@ if (isVercel) {
 app.post('/api/submit', (req, res) => {
   const { name, email, message } = req.body;
 
-  // // Email options
-  // const mailOptions = {
-  //   // from: EMAIL_USER,
-  //   // to: EMAIL_USER, // Send to yourself
-  //   // replyTo: email,
-  //   from: process.env.EMAIL_USER,
-  //   to: process.env.EMAIL_USER,
-  //   replyTo: email,
-  //   subject: `New Contact Form Submission from ${name}`,
-  //   text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  //   html: `
-  //     <h2>New Contact Form Submission</h2>
-  //     <p><strong>Name:</strong> ${name}</p>
-  //     <p><strong>Email:</strong> ${email}</p>
-  //     <p><strong>Message:</strong></p>
-  //     <p>${message.replace(/\n/g, '<br>')}</p>
-  //   `
-  // };
+  // Email options
+  const mailOptions = {
+    // from: EMAIL_USER,
+    // to: EMAIL_USER, // Send to yourself
+    // replyTo: email,
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    replyTo: email,
+    subject: `New Contact Form Submission from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    html: `
+      <h2>New Contact Form Submission</h2>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong></p>
+      <p>${message.replace(/\n/g, '<br>')}</p>
+    `
+  };
 
-  // // Send email
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //   //   // console.error('Error sending email:', error);
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+    //   // console.error('Error sending email:', error);
     //   // return res.status(500).json({ 
     //     // success: false, 
     //     // message: 'Error sending your message. Please try again later.' 
@@ -82,16 +82,16 @@ app.post('/api/submit', (req, res) => {
     // //   success: true, 
     // //   message: 'Thank you for your message! I will get back to you soon.' 
     // res.sendFile(path.join(__dirname, 'contact.html'));
-  //   console.error('Error:', error);
-  //     return res.redirect(303, '/public/404.html');
-  //   }
-  //   return res.redirect(303, '/public/contact.html');
-  //   });
-  // });
+    console.error('Error:', error);
+      return res.redirect(303, '/public/404.html');
+    }
+    return res.redirect(303, '/public/contact.html');
+    });
+  });
 
 
 
 // Start server
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
- });
+// });
