a414d6a336ad19e4fc5e240d98e28527ecab76f2
Svilyear <villiersteven@gmail.com>
2025-04-24 16:05:18 +0100

    updated mail options

--- public/api/submit.js
+++ public/api/submit.js
@@ -15,10 +15,9 @@ export default async function handler(req, res) {
       pass: process.env.EMAIL_PASS
     }
   });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
+  await transporter.sendMail({
+    from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+    to: process.env.PERSONAL_EMAIL,
     replyTo: email,
     subject: `New Contact Form Submission from ${name}`,
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
@@ -28,7 +27,7 @@ export default async function handler(req, res) {
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Message:</strong><br>${message.replace(/\n/g, '<br>')}</p>
     `
-  };
+  });
 
   // try {
   //   await transporter.sendMail(mailOptions);
